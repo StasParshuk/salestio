c81ded0dd9a2ebbf63c2f6234a150a4b8dc51bda
stanislav_parshuk <stanislav.parshuk@forforce.com>
2024-02-04 19:29:28 +0200

    create logic

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 
+//тут в идеале фос рест бандл и  нормальное версионирование
 #[Route('/api/v1/cart')]
 class CartController extends AbstractController
 {
